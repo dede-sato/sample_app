d930e987d899456859e48654e1fe24c116982618
Rina-8 <rina8da@gmail.com>
2021-04-10 14:53:40 +0900

    第7章 done

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '7ced078d74793a58f860d7be23ca1abd11bcd4d099d3445330f1b370e8ed521cbbab1cda94e97e6ee4bdcc1bf60686fd5b8fa3ea9e125089cb712e22c01c273f'
